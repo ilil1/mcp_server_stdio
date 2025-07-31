7eb397d5b4849f1938cf51000760ee32ba2bd1d9
김건우 <free.rjs102@gmail.com>
2025-07-31 14:40:44 +0900

    stdio 로 변경

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("mcp_project")
@@ -11,8 +14,16 @@ def add(a: int, b: int) -> int:
 def validate_test_sc() -> str:
     return "성공"
 
+# if __name__ == "__main__":
+#     try:
+#         port = int(os.environ.get('PORT', 8000))
+#         app = mcp.streamable_http_app()
+#         uvicorn.run(app, host="0.0.0.0", port=port, log_level="info", timeout_keep_alive=60)
+#     except Exception as e:
+#         raise
+
 if __name__ == "__main__":
     import sys
     print("Starting MCP server...", file=sys.stderr)
-    mcp.run()
+    mcp.run("stdio")
     print("Server stopped", file=sys.stderr)
